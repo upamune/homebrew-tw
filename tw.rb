884e5e45e526d74c6a8c7c53b57824dcc057ec8a
upamune <jajkeqos@gmail.com>
2015-08-09 00:42:58 +0900

    Remove chmod

--- tw.rb
+++ tw.rb
@@ -5,13 +5,9 @@ class Tw < Formula
   homepage 'https://github.com/upamune/tw'
   url 'https://github.com/upamune/tw.git', :tag => "v#{HOMEBREW_TW_VERSION}"
   version HOMEBREW_TW_VERSION
-  head 'https://github.com/upamune/tw.git', :branch => 'master'
-
-  depends_on 'go' => :build
 
   def install
     system 'curl','-o','tw',"https://github.com/upamune/tw/releases/download/v#{HOMEBREW_TW_VERSION}/tw_darwin_amd64"
-    system 'chmod','+x','tw'
     bin.install 'tw'
   end
 end
