749bc5d505a3903d8e0b906cba78afac9fdd063e
upamune <jajkeqos@gmail.com>
2015-08-09 12:04:06 +0900

    Change install method

--- tw.rb
+++ tw.rb
@@ -5,9 +5,20 @@ class Tw < Formula
   homepage 'https://github.com/upamune/tw'
   url 'https://github.com/upamune/tw.git', :tag => "v#{HOMEBREW_TW_VERSION}"
   version HOMEBREW_TW_VERSION
+  head 'https://github.com/upamune/tw.git', :branch => 'master'
+
+  depends_on 'go' => :build
 
   def install
-    system 'curl','-o','tw',"https://github.com/upamune/tw/releases/download/v#{HOMEBREW_TW_VERSION}/tw_darwin_amd64"
+    ENV['GOPATH'] = buildpath
+    system 'go', 'get', 'github.com/codegangsta/cli'
+    system 'go', 'get', 'github.com/ChimeraCoder/anaconda'
+    system 'go', 'get', 'github.com/mitchellh/go-homedir'
+    system 'go', 'get', 'github.com/mrjones/oauth'
+    system 'go', 'get', 'github.com/skratchdot/open-golang/open'
+    system 'go', 'get', 'github.com/mgutz/ansi'
+    system 'go', 'get', 'github.com/BurntSushi/toml'
+    system 'go', 'build', '-o', 'tw'
     bin.install 'tw'
   end
 end
